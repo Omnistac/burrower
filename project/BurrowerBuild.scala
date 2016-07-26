df8a8124cb647246b09ae0c5e2ed9a23c5214059
Ray Chen <raysmail@gmail.com>
2016-07-26 15:02:49 -0400

    make docker build

--- project/BurrowerBuild.scala
+++ project/BurrowerBuild.scala
@@ -1,5 +1,10 @@
 import sbt._
 import Keys._
+import com.typesafe.sbt.packager.archetypes.JavaServerAppPackaging
+import com.typesafe.sbt.packager.docker.DockerPlugin.autoImport._
+import com.typesafe.sbt.packager.docker.{DockerPlugin, _}
+import com.typesafe.sbt.packager.linux.LinuxPlugin.autoImport._
+import sbt.dsl._
 
 object BurrowerBuild extends Build {
 
@@ -13,13 +18,38 @@ object BurrowerBuild extends Build {
   lazy val project = Project("burrower", file("."))
     .settings(buildSettings: _*)
     .settings(
-      libraryDependencies ++= Seq(
-        Libraries.playJson,
-        Libraries.scalaLogging,
-        Libraries.logbackClassic,
-        Libraries.scalaJ,
-        Libraries.influxDb
-      )
+    libraryDependencies ++= Seq(
+      Libraries.playJson,
+      Libraries.scalaLogging,
+      Libraries.logbackClassic,
+      Libraries.scalaJ,
+      Libraries.influxDb
     )
+  )
     .settings(scalaVersion := "2.11.7")
+
+  mainClass in Compile :=Some("com.github.splee.burrower.OffsetMonitor")
+
+  enablePlugins(JavaServerAppPackaging)
+
+  enablePlugins(DockerPlugin)
+
+  mappings in Universal <++= (packageBin in Compile, sourceDirectory) map { (_, src) =>
+    packageMapping(
+      (src / "main" / "resources") -> "conf"
+    ).withContents().mappings.map {
+      case (f, p) => (f.asInstanceOf[java.io.File], p)
+    }.toSeq
+  }
+
+  dockerRepository := Some("omnistac-docker-local.jfrog.io/omnistac")
+
+  dockerBaseImage := "frolvlad/alpine-oraclejdk8"
+
+  dockerCommands := dockerCommands.value.flatMap{   case cmd@Cmd("FROM",_) => List(cmd, Cmd("RUN", "apk update && apk add bash"))   case other => List(other) }
+
+  dockerUpdateLatest := true
+
+  version in Docker := sys.props.getOrElse("tag", default = version.value)
+
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,1 +1,3 @@
+addSbtPlugin("com.typesafe.sbt" % "sbt-native-packager" % "1.0.3")
+
 addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.11.2")
